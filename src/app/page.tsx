1f0f85a86040fa060042d163d707b7bc4ef937cc
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2025-06-23 15:48:08 +0300

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="">Hello World</h1>
-
       <Button variant={"custom"}>Test Button</Button>
     </div>
   );
