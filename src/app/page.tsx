36e1187581f6430e6b657e2fdab9d847b4b9383b
Kaan Ayyıldız <ykaanayyildiz@gmail.com>
2025-06-25 17:43:52 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
